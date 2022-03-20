5153cee785126957d39b1f52b91efd12dca3066c
Franco <francoperaltagg@gmail.com>
2022-03-20 15:23:46 -0300

    propTypes y defaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,14 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 //import React, { Fragment } from "react";
 
-const PrimeraApp = ( { saludo } ) => {
+const PrimeraApp = ( { saludo, subtitulo = "Soy un subtitulo" } ) => {
+
+
 
     return (
         <>
             <h1> { saludo } </h1>
-            <p>Mi primera app</p>
+            <p> { subtitulo }</p>
         </>
     );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired //puede ser del tipo que queramos, string, boolean, lo que sea. Y el isRequired lo vuelve obligatorio
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: "Soy un subtitulo con defaultProps"
 }
 
 export default PrimeraApp;
